3e9787a084e094536b7640bf3d8fc6556d9fe8dd
d3xian <515224526@qq.com>
2020-04-13 00:19:07 +0800

    fix:site

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,9 @@
-import Taro, { Component } from '@tarojs/taro'
-import Index from './pages/index'
-
-import './app.less'
+import Taro, { Component } from "@tarojs/taro";
+import Index from "./pages/index";
+import Mine from "./pages/mine";
+import "taro-ui/dist/style/index.scss"; // 全局引入一次即可
+import "./app.less";
+import "../assets/icon.css"
 
 // 如果需要在 h5 环境中开启 React Devtools
 // 取消以下注释：
@@ -10,34 +12,46 @@ import './app.less'
 // }
 
 class App extends Component {
-
   config = {
-    pages: [
-      'pages/index/index'
-    ],
+    pages: ["pages/index/index", "pages/mine/index"],
     window: {
-      backgroundTextStyle: 'light',
-      navigationBarBackgroundColor: '#fff',
-      navigationBarTitleText: 'WeChat',
-      navigationBarTextStyle: 'black'
-    }
-  }
+      backgroundTextStyle: "light",
+      navigationBarBackgroundColor: "#fff",
+      navigationBarTitleText: "WeChat",
+      navigationBarTextStyle: "black",
+    },
+    tabBar: {
+      list: [
+        {
+          selectedIconPath: '\xe69a',
+          iconPath: '\xe64c',
+          pagePath: "pages/index/index",
+          text: "首页",
+        },
+        {
+          selectedIconPath: '\xe618',
+          iconPath: '\xe613',
+          pagePath: "pages/mine/index",
+          text: "我的",
+        },
+      ],
+    },
+    sitemapLocation: "sitemap.json",
+  };
 
-  componentDidMount () {}
+  componentDidMount() {}
 
-  componentDidShow () {}
+  componentDidShow() {}
 
-  componentDidHide () {}
+  componentDidHide() {}
 
-  componentDidCatchError () {}
+  componentDidCatchError() {}
 
   // 在 App 类中的 render() 函数没有实际作用
   // 请勿修改此函数
-  render () {
-    return (
-      <Index />
-    )
+  render() {
+    return <Index />;
   }
 }
 
-Taro.render(<App />, document.getElementById('app'))
+Taro.render(<App />, document.getElementById("app"));
