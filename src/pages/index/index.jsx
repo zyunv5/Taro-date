2adb90c5bf2b47cf620d16ea5cad8e15595450f1
d3xian <515224526@qq.com>
2020-04-20 00:54:54 +0800

    style change

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -12,6 +12,18 @@ export default class Index extends Component {
     super(props);
     this.state = {
       canIUse: wx.canIUse("button.open-type.getUserInfo"),
+      listData: [
+        { id: 0, name: "妈妈生日", date: "5" },
+        { id: 1, name: "爸爸生日", date: "15" },
+        { id: 2, name: "妻子生日", date: "25" },
+        { id: 3, name: "来京日子", date: "35" },
+        { id: 4, name: "结婚纪念日", date: "45" },
+        { id: 5, name: "妈妈生日", date: "55" },
+        { id: 6, name: "爸爸生日", date: "65" },
+        { id: 7, name: "妻子生日", date: "75" },
+        { id: 8, name: "来京日子", date: "85" },
+        { id: 9, name: "结婚纪念日", date: "95" },
+      ],
     };
   }
 
@@ -53,10 +65,10 @@ export default class Index extends Component {
   };
 
   render() {
-    const { canIUse } = this.state;
+    const { canIUse, listData } = this.state;
     return (
       <View className="index">
-        {canIUse ? (
+        {/* {canIUse ? (
           <button
             open-type="getUserInfo"
             onGetUserInfo={(userInfo) => this.onGetUserInfo(userInfo)}
@@ -65,10 +77,23 @@ export default class Index extends Component {
           </button>
         ) : (
           <view>请升级微信版本</view>
-        )}
+        )} */}
         <Search />
-        <View onClick={() => this.goRouteBirthday()}>生日</View>
-        <View>纪念日</View>
+        <View className="index-add">
+          <View className="add-item" onClick={() => this.goRouteBirthday()}>
+            新增生日
+          </View>
+          <View className="add-item">新增纪念日</View>
+        </View>
+        <View className="index-list">
+          {listData.map((item, index) => {
+            return (
+              <View className="list-item" key={item.id}>
+                {item.name}
+              </View>
+            );
+          })}
+        </View>
       </View>
     );
   }
--- src/pages/mine/index.jsx
+++ src/pages/mine/index.jsx
@@ -1,29 +1,34 @@
-import Taro, { Component } from '@tarojs/taro'
-import { View, Text,Image} from '@tarojs/components'
-import './index.less'
+import Taro, { Component } from "@tarojs/taro";
+import { View, Text, Image } from "@tarojs/components";
+import "./index.less";
 
 export default class Index extends Component {
-
   config = {
-    navigationBarTitleText: '我的'
-  }
+    navigationBarTitleText: "我的",
+  };
 
-  componentWillMount () { }
+  componentWillMount() {}
 
-  componentDidMount () { }
+  componentDidMount() {}
 
-  componentWillUnmount () { }
+  componentWillUnmount() {}
 
-  componentDidShow () { }
+  componentDidShow() {}
 
-  componentDidHide () { }
+  componentDidHide() {}
 
-  render () {
+  render() {
     return (
-      <View className='mine'>
-        <Image className="mine-image"/>
-        <View className="mine-name">雨神</View>
+      <View className="mine">
+        <View className="mine-info">
+          <Image className="info-portrait" />
+          <View className="info-name">雨神</View>
+        </View>
+        <View className="mine-list">
+          <View className="list-item">生日列表</View>
+          <View className="list-item">纪念日列表</View>
+        </View>
       </View>
-    )
+    );
   }
 }
